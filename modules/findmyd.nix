4586eea47194d3603bd7146156c68f4fa9b5bde6
FUKUMOTO Yuki <rk76fewf@gmail.com>
2026-04-27 00:01:09 +0900

    Add FMF key injection for friends tracking

--- modules/findmyd.nix
+++ modules/findmyd.nix
@@ -5,9 +5,11 @@ let
   home = config.users.users.${user}.home;
   findmyd = inputs.findmyd.packages.${pkgs.system}.default;
   cacheDir = "${home}/Library/Caches/com.apple.findmy.fmipcore";
+  fmfCacheDir = "${home}/Library/Caches/com.apple.findmy.fmfcore";
   dbDir = "${home}/.local/share/findmyd";
   dbPath = "${dbDir}/findmyd.db";
-  keyPath = "${dbDir}/fmip.key";
+  fmipKeyPath = "${dbDir}/fmip.key";
+  fmfKeyPath = "${dbDir}/fmf.key";
   op = pkgs._1password-cli;
 in {
   system.activationScripts.setupFindmyd.text = lib.mkAfter ''
@@ -19,10 +21,14 @@ in {
       ProgramArguments = [
         "/bin/bash" "-c"
         ''
-          ${op}/bin/op read --out-file "${keyPath}" --force "op://Personal/findmy/FMIPDataManager.bplist"
-          chmod 600 "${keyPath}"
-          export FMIP_KEY_FILE="${keyPath}"
+          ${op}/bin/op read --out-file "${fmipKeyPath}" --force "op://Personal/findmy/FMIPDataManager.bplist"
+          chmod 600 "${fmipKeyPath}"
+          ${op}/bin/op read --out-file "${fmfKeyPath}" --force "op://Personal/findmy/FMFDataManager.bplist"
+          chmod 600 "${fmfKeyPath}"
+          export FMIP_KEY_FILE="${fmipKeyPath}"
+          export FMF_KEY_FILE="${fmfKeyPath}"
           export CACHE_DIR="${cacheDir}"
+          export FMF_CACHE_DIR="${fmfCacheDir}"
           export DB_PATH="${dbPath}"
           export POLL_INTERVAL=2
           export PORT=8080
